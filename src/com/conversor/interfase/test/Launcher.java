55ef096366f21047ecfa416110ef06cc400c5c83
Cesar Ordaz <cesar.ordazv@gmail.com>
2023-07-07 11:39:01 -0600

    More swing testing stuff

--- src/com/conversor/interfase/test/Launcher.java
+++ src/com/conversor/interfase/test/Launcher.java
@@ -2,12 +2,11 @@ package com.conversor.interfase.test;
 
 import javax.swing.SwingUtilities;
 
-import com.conversor.interfase.test.MainFrame;
-
 public class Launcher {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             MainFrame main = new MainFrame();
+            main.show();
         });
     }
 }
--- src/com/conversor/interfase/test/MainFrame.java
+++ src/com/conversor/interfase/test/MainFrame.java
@@ -1,10 +1,20 @@
 package com.conversor.interfase.test;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 import javax.swing.WindowConstants;
 
 public class MainFrame {
     private JFrame frame;
+    private JPanel panel;
+    private JButton button;
 
     public MainFrame() {
         initialize();
@@ -16,7 +26,46 @@ public class MainFrame {
         frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         frame.setSize(800, 500);
         frame.setLocationRelativeTo(null);
+
+        panel = new JPanel();
+
+        button = createButton("Button", ".env/icons/print.png");
+        panel.add(button);
+
+        frame.add(panel);
+    }
+
+    public JButton createButton(String text, String file) {
+        JButton myButton = new JButton(text);
+        myButton.setFocusable(false);
+
+        ImageIcon printIcon = new ImageIcon(file);
+        myButton.setIcon(printIcon);
+        myButton.setIconTextGap(10);
+
+        myButton.setToolTipText("Algo de texto para el botón");
+
+        myButton.setFont(new Font("Arial", Font.PLAIN, 16));
+
+        myButton.setMargin(new Insets(20, 20, 20, 20));
+
+        myButton.addActionListener((ActionEvent event) -> System.out.println("¡Hola! Presionaste el botón."));
+
+        //myButton.setEnabled(false);
         
-        frame.setVisible(true);
+        myButton.setMnemonic(KeyEvent.VK_B);
+
+        myButton.doClick();
+
+        myButton.setVerticalTextPosition(SwingConstants.CENTER);
+        myButton.setHorizontalTextPosition(SwingConstants.LEFT);
+
+        myButton.setPreferredSize(new Dimension(200, 75));
+
+        return myButton;
+    }
+
+    public void show() {
+        this.frame.setVisible(true);
     }
 }
